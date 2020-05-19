4ef63e20e0f03702b0d1cfbd3c252f92b784623f
ArtemReva <artemu78@yandex.ru>
2020-05-19 23:46:01 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactTSPopup
-Created with CodeSandbox
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+
+import "./index.css";
+import { Root } from "./Root";
+
+const rootElement = document.getElementById("root");
+
+render(<Root />, rootElement);
